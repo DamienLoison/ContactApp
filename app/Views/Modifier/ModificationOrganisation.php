08b88c2e527487de82cca5423eedef5077093af9
DamienLoison <damien.loison.pro@gmail.Com>
2022-03-10 09:37:32 +0100

    Version EXP B:1.0.11-14
    ->ajout du nom de la ville dans l'affichage des organisations
    ->ajout du nom de la ville dans la modification de l'organisation

--- app/Views/Modifier/ModificationOrganisation.php
+++ app/Views/Modifier/ModificationOrganisation.php
@@ -2,12 +2,10 @@
 <html lang="fr-Fr">
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Modifier organisation</title>
+        <?php echo view('template/header.php') ?>
     </head>
     <body>
-        <div class="nav">
-            <?php echo view('template/header.php') ?>
-        </div>
         <div class="container mt-3">
             <div class="row">
                 <div class="col-sm-4 offset-4 my-3">
@@ -15,15 +13,19 @@
                     <form action="<?= base_url('Recherche/update_organisation/' . $organisation['ID_Organisation']); ?>" method="POST">
                         <div class="mb-3 mt-3">
                             <label>Nom de l'organisation :</label>
-                            <input type="text" name="Nom_Organisation" value="<?= $organisation['Nom_Organisation'] ?>" class="form-control"  placeholder="Entrer le nom" required>
+                            <input type="text" name="Nom_Organisation" value="<?= $organisation['Nom_Organisation'] ?>" class="form-control"  placeholder="Entrer le nom de l'organisation" required>
                         </div>
                         <div class="mb-3 mt-3">
                             <label>Adresse de l'organisation :</label>
-                            <input type="text" name="Adresse_Organisation" value="<?= $organisation['Adresse_Organisation'] ?>" class="form-control"  placeholder="Entrer le prénom" required>
+                            <input type="text" name="Adresse_Organisation" value="<?= $organisation['Adresse_Organisation'] ?>" class="form-control"  placeholder="Entrer l'Adresse de l'organisation" required>
+                        </div>
+                        <div class="mb-3 mt-3">
+                            <label>Ville de l'organisation :</label>
+                            <input type="text" name="Ville_Organisation" value="<?= $organisation['Ville_Organisation'] ?>" class="form-control"  placeholder="Entrer la Ville de l'organisation" required>
                         </div>
                         <div class="mb-3 mt-3">
                             <label>Mail de l'organisation :</label>
-                            <input type="text" name="Mail_Organisation" value="<?= $organisation['Mail_Organisation'] ?>" class="form-control"  placeholder="Entrer le mail" required>
+                            <input type="text" name="Mail_Organisation" value="<?= $organisation['Mail_Organisation'] ?>" class="form-control"  placeholder="Entrer le mail de l'organisation" required>
                         </div>
                         <div class="mb-3 mt-3">
                             <label>Site de l'organisation :</label>
@@ -31,7 +33,7 @@
                         </div>
                         <div class="mb-3 mt-3">
                             <label>numéro de téléphone de l'organisation :</label>
-                            <input type="text" name="Telephone_Organisation" value="<?= $organisation['Telephone_Organisation'] ?>" class="form-control" placeholder="Entrer le numéro de téléphone" minlength="10" maxlength="10" required>
+                            <input type="text" name="Telephone_Organisation" value="<?= $organisation['Telephone_Organisation'] ?>" class="form-control" placeholder="Entrer le numéro de téléphone de l'organisation" minlength="10" maxlength="10" required>
                         </div>
                         <button type="submit" class="btn btn-primary btn-block btn-lg">Valider</button>
                         <form action="<?= base_url('Recherche/toute_les_organisations'); ?>" method="POST">
