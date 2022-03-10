4311fc33b8c020bc02c20185081d04784ffcc2eb
DamienLoison <damien.loison.pro@gmail.Com>
2022-03-10 15:15:01 +0100

    Version EXP B:1.0.11-21 correction
    ->correction

--- app/Views/Reste/Contacter.php
+++ app/Views/Reste/Contacter.php
@@ -11,15 +11,20 @@
         #info{
             height: 300px;
             width: 500px;
-            border: solid rgb(84,84,84) 2px;
+            margin-left: 30%;
+            margin-right: 50%;
         }
     </style>
     <body>
         <div class="bg-dark text-white text-center">
             <h1>ME CONTACTER</h1>
             <div id="info" class="bg-dark text-white">
-                Adresse mail où me contacter : damien.loison.pro@gmail.Com <br>
-                Lieu pour me contacter en cas de problème avec l'applciation <a href="https://github.com/DamienLoison/ContactApp/issues">GITHUB</a>
+                <br><br>
+                Adresse mail où me contacter : damien.loison.pro@gmail.Com 
+                <br><br>
+                _____________________________
+                <br><br>
+                Lieu pour me contacter en cas de problème avec l'application <a href="https://github.com/DamienLoison/ContactApp/issues">GITHUB</a>
             </div>
         </div>
     </body>
