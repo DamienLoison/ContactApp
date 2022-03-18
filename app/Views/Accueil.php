aad288a4e55ae38beafae5eeede51ef2f4745d6e
DamienLoison <damien.loison.pro@gmail.Com>
2022-03-18 16:10:15 +0100

    DEV v1.0.0
    corrections mineures

--- app/Views/Accueil.php
+++ app/Views/Accueil.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width-device-width, initial-scale-1.0">
         <link rel="stylesheet" type="text/css" href="Style.css" media="all" />
+        <!--ACTUALISE LA PAGE TOUTE LES MINUTES-->
         <meta http-equiv="refresh" content="60" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
@@ -51,11 +52,6 @@
                     }
                 }
             }
-
-            //  ACTUALISE AUTOMATIQUEMENT LA PAGE
-            var nb = 10;
-
-            timer = window.setTimeout(" window.refresh();", 1000 * nb, "JavaScript");
         </script>
         <div class="Accueil">
             <div class="text-center bg-dark text-white">
--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -34,7 +34,7 @@
             <a class="nav-link" href="/Accueil/note_patch" tabindex="-1">Note de patch</a>
         </li>
         <li class="nav-item ms-auto">
-            Beta version : B:1.0.12
+            Beta v1.0.0
         </li>
     </ul>
 </html>
