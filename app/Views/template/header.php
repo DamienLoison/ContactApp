8fde990d78ca5b4039023c194970a5cc1ec4590e
DamienLoison <damien.loison.pro@gmail.Com>
2022-03-22 15:44:22 +0100

    DEV v1.0.16
    mise à jour du HEADER
    ajout d'image pour remplacer le texte

--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -33,24 +33,30 @@
         <li class="nav-item">
             <a class="nav-link" href="/Accueil/note_patch" tabindex="-1">Note de patch</a>
         </li>
-        <li class="nav-item text-white">
-            <?php
-            $session = session();
-            if ($session->get('user_name') == null) {
-                
-            } else {
-                echo "Connecté en tant que : " . $session->get('user_name');
-            }
-            ?>
-        </li>
         <li class="nav-item ms-auto">
-            <a class="nav-link" href="/LoginRegisterController/">Inscription</a>
+            <a class="nav-link" href="/LoginRegisterController/"><img src="https://cdn-icons.flaticon.com/png/512/1144/premium/1144695.png?token=exp=1647960567~hmac=a3bb766fd42118f14e7fd07544414178" width="30px" height="30px" alt="alt"/></a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="/LoginRegisterController/login">Connexion</a>
+            <a class="nav-link" href="/LoginRegisterController/login"><img src="https://cdn-icons.flaticon.com/png/512/310/premium/310767.png?token=exp=1647960504~hmac=f441cb3299e32b99dca8a05a92530fd1" width="30px" height="30px" alt="connexion"/></a>
         </li>
         <li class="nav-item ">
-            <a class="nav-link text-danger" href="/LoginRegisterController/logout">Déconnexion</a>
+            <a class="nav-link text-danger" href="/LoginRegisterController/logout"><img src="https://cdn-icons.flaticon.com/png/512/3686/premium/3686918.png?token=exp=1647960290~hmac=927c1612f008f2c25644f51918557ae3" width="30px" height="30px" alt="déconnexion"/></a>
+        </li>
+
+
+
+        <li class="nav-item text-white">
+            <a class="nav-link text-white">
+                <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="user" width="30px" height="30px"/>
+                <?php
+                $session = session();
+                if ($session->get('user_name') == null) {
+                    echo "connectez-vous ! " . " ";
+                } else {
+                    echo " " . $session->get('user_name');
+                }
+                ?>
+            </a>
         </li>
         <li class="nav-item">
             v1.0.1
